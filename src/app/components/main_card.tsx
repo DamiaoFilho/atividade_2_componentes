d5a01883cd7e69650a28c30f3b1f7ba479555b7e
DamiaoFilho <damiaofilho0123@gmail.com>
2025-05-04 16:25:58 -0300

    exercise 2 finished

--- src/app/components/main_card.tsx
+++ src/app/components/main_card.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import Button from "./button";
 
-export default function MainCard() {
+interface Props{
+  addTask: (name:string, description:string) => void
+}
+
+export default function MainCard({addTask}: Props) {
   return (
     <div className="flex flex-col gap-5 border-gray-300 border-1 rounded-md w-3xl p-5">
       <h1 className="text-black font-bold text-xl">Nova Tarefa</h1>
-      <form className="flex flex-col gap-5">
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        addTask("nova tarefa", "nova descrição")
+      }} className="flex flex-col gap-5">
         <input placeholder="Título da Tarefa"
             className="border-gray-300 border-1 p-4 rounded-md w-[100%] h-5 text-gray-900 text-md"/>
         <textarea 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,30 @@
-import Image from "next/image";
+"use client"
 import MainCard from "./components/main_card";
 import Card from "./components/card";
+import { useState } from "react";
+interface Task{
+  title: string;
+  description: string;
+}
 
 export default function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = (name: string, description: string) => {
+    setTasks([...tasks, {title:name, description:description}])
+  }
+
   return (
     <div className="flex flex-col gap-5 justify-center items-center min-h-screen min-w-screen">
       <h1 className="text-black font-bold p-2 font text-3xl">
         Lista de Tarefas
       </h1>
-      <MainCard></MainCard>
+      <MainCard addTask={addTask}></MainCard>
+
       <div className="flex flex-row justify-around w-3xl gap-2">
-        <Card
-          name="Exemplo de Tarefa"
-          description="Esta é uma tarefa de exemplo. Você pode marcar como concluída, editar ou excluir."
-          ></Card>
-        <Card
-          name="teste"
-          description="teste"
-        ></Card>
+        {tasks.map((task, index) => (
+          <Card key={index} name={task.title} description={task.description} />
+        ))}
       </div>
     </div>
   );
